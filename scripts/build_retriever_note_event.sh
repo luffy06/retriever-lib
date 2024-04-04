60f5d2e3d58a473c3dd626084f860a34aa6d7007
luffy06 <534427411@qq.com>
2024-04-04 22:03:09 +0800

    update scripts and builder

--- scripts/build_retriever_note_event.sh
+++ scripts/build_retriever_note_event.sh
@@ -3,18 +3,18 @@ ENCODER_NAME=bert-large-uncased
 ENCODER_PATH=/mnt/wsy/models/$ENCODER_NAME
 PROJECT_DIR=$(dirname "$(dirname "$(realpath "$0")")")
 
-# # Get embeddings
-# CUDA_VISIBLE_DEVICES=$DEVICE \
-#   CUDA_LAUNCH_BLOCKING=1 \
-#   python $PROJECT_DIR/src/faisslib/build_retriever.py \
-#     --data_dir $PROJECT_DIR/corpus/formatted/note-event \
-#     --model_path $ENCODER_PATH \
-#     --output_dir $PROJECT_DIR/metadata/note-event-$ENCODER_NAME \
-#     --device_id 0 \
-#     --do_chunk \
-#     --do_encode \
-#     --num_chunks_per_file 100000 \
-#     --batch_size 128 \
+# Get embeddings
+CUDA_VISIBLE_DEVICES=$DEVICE \
+  CUDA_LAUNCH_BLOCKING=1 \
+  python $PROJECT_DIR/src/faisslib/build_retriever.py \
+    --data_dir $PROJECT_DIR/corpus/formatted/note-event \
+    --model_path $ENCODER_PATH \
+    --output_dir $PROJECT_DIR/metadata/note-event-$ENCODER_NAME \
+    --device_id 0 \
+    --do_chunk \
+    --do_encode \
+    --num_chunks_per_file 100000 \
+    --batch_size 128 \
 
 echo 'Build DB and index based on 20M note-event data'
 CUDA_VISIBLE_DEVICES=$DEVICE \
@@ -27,7 +27,7 @@ CUDA_VISIBLE_DEVICES=$DEVICE \
     --build_db \
     --build_index \
     --build_size 20000000 \
-    --max_train_size 1000000 \
+    --max_train_size 2000000 \
     --index_type IVF65536_HNSW32,PQ64 \
     --metric_type L2 \
     --sub_index_size 10000 \
@@ -42,7 +42,7 @@ CUDA_VISIBLE_DEVICES=$DEVICE \
     --device_id -1 \
     --build_db \
     --build_index \
-    --max_train_size 1000000 \
+    --max_train_size 2000000 \
     --index_type IVF65536_HNSW32,PQ64 \
     --metric_type L2 \
     --sub_index_size 10000 \
--- scripts/build_retriever_wikipedia.sh
+++ scripts/build_retriever_wikipedia.sh
@@ -1,19 +1,20 @@
 DEVICE=0
-ENCODER_PATH=/mnt/wsy/models/bert-base-uncased
+ENCODER_NAME=bert-large-uncased
+ENCODER_PATH=/mnt/wsy/models/$ENCODER_NAME
 PROJECT_DIR=$(dirname "$(dirname "$(realpath "$0")")")
 
-# # Get embeddings
-# CUDA_VISIBLE_DEVICES=$DEVICE \
-#   CUDA_LAUNCH_BLOCKING=1 \
-#   python $PROJECT_DIR/src/faisslib/build_retriever.py \
-#     --data_dir $PROJECT_DIR/corpus/formatted/wikipedia-split \
-#     --model_path $ENCODER_PATH \
-#     --output_dir $PROJECT_DIR/metadata/wikipedia-split \
-#     --device_id 0 \
-#     --do_chunk \
-#     --do_encode \
-#     --num_chunks_per_file 100000 \
-#     --batch_size 128 \
+# Get embeddings
+CUDA_VISIBLE_DEVICES=$DEVICE \
+  CUDA_LAUNCH_BLOCKING=1 \
+  python $PROJECT_DIR/src/faisslib/build_retriever.py \
+    --data_dir $PROJECT_DIR/corpus/formatted/wikipedia-split \
+    --model_path $ENCODER_PATH \
+    --output_dir $PROJECT_DIR/metadata/wikipedia-split-$ENCODER_NAME \
+    --device_id 0 \
+    --do_chunk \
+    --do_encode \
+    --num_chunks_per_file 100000 \
+    --batch_size 128 \
 
 echo 'Build DB and index based on 60M wikipedia data'
 CUDA_VISIBLE_DEVICES=$DEVICE \
@@ -21,12 +22,12 @@ CUDA_VISIBLE_DEVICES=$DEVICE \
   python $PROJECT_DIR/src/faisslib/build_retriever.py \
     --data_dir $PROJECT_DIR/corpus/formatted/wikipedia-split \
     --model_path $ENCODER_PATH \
-    --output_dir $PROJECT_DIR/metadata/wikipedia-split \
+    --output_dir $PROJECT_DIR/metadata/wikipedia-split-$ENCODER_NAME \
     --device_id -1 \
-    --build_size 60000000 \
     --build_db \
     --build_index \
-    --max_train_size 1000000 \
+    --build_size 20000000 \
+    --max_train_size 2000000 \
     --index_type IVF65536_HNSW32,PQ64 \
     --metric_type L2 \
     --sub_index_size 10000 \
@@ -37,11 +38,12 @@ CUDA_VISIBLE_DEVICES=$DEVICE \
   python $PROJECT_DIR/src/faisslib/build_retriever.py \
     --data_dir $PROJECT_DIR/corpus/formatted/wikipedia-split \
     --model_path $ENCODER_PATH \
-    --output_dir $PROJECT_DIR/metadata/wikipedia-split \
+    --output_dir $PROJECT_DIR/metadata/wikipedia-split-$ENCODER_NAME \
     --device_id -1 \
     --build_db \
     --build_index \
-    --max_train_size 1000000 \
+    --build_size 40000000 \
+    --max_train_size 2000000 \
     --index_type IVF65536_HNSW32,PQ64 \
     --metric_type L2 \
     --sub_index_size 10000 \
